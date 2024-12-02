6f3c5b472c07958e30a6ecb072c37e8f34242cbc
Satyam020905 <satyamkr922@gmail.com>
2024-12-02 11:04:04 +0530

    Content security policy

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const port = 3000
 
 
 
-// app.use(cors());
+app.use(cors());
 const corsOptions = {
   origin: ['https://convert-app-seven.vercel.app', 'http://localhost:3000'], // Allow requests only from your frontend
   methods: ['GET', 'POST'],       // Specify allowed HTTP methods
